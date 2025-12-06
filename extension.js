6550120a5e83f7e80cc1b4523d5917802108bc27
Claudio Benghi <claudio.benghi@gmail.com>
2025-12-06 00:25:37 +0000

    Added 3 levels of feedback and better highlighting

--- extension.js
+++ extension.js
@@ -15,8 +15,9 @@ function activate(context) {
     const text = document.getText();
     const lines = text.split(/\r?\n/);
 
-    const todoRegex = /(TODO|FIXME):\s*(.*)$/i;
-    const nextRegex = /(NEXT|WORK):\s*(.*)$/i;
+    const nextRegex = /(\{>>\s*|\(\s*)?\b(NEXT|WORK):\s*(.*)$/i;
+    const todoRegex = /(\{>>\s*|\(\s*)?\b(TODO|FIXME|XXX):\s*(.*)$/i;
+    const infoRegex = /(\{>>\s*|\(\s*)?\b(PLAN|INFO):\s*(.*)$/i;
     var lineCount = 0;
     lines.forEach(thisLine => {
       let match;
@@ -28,20 +29,43 @@ function activate(context) {
         // fs.appendFileSync("c:\\Data\\AcademicDocs\\.markdownlint\\custom-rules\\debug.log",
         //   `line ${lineCount} (${rangeStart} + ${rangeLength})\n`);
         tCode = "CM01";  
-        tMessage = `${match[0]}`;
         tSeverity = vscode.DiagnosticSeverity.Error;
       }
       else if ((match = todoRegex.exec(thisLine)) !== null)
       {
         tCode = "CM02";  
-        tMessage = `${match[0]}`;
         tSeverity = vscode.DiagnosticSeverity.Warning;
       }
+      else if ((match = infoRegex.exec(thisLine)) !== null)
+      {
+        tCode = "CM03";  
+        tSeverity = vscode.DiagnosticSeverity.Information;
+      }
       if (match !== null) {
-        var rangeStart = match.index || 0;
-        var rangeLength = match[0].length || 0;
+        opening = match[1] || "";
+        var rangeStart = match.index + opening.length || 0;
+        tMessage = `${match[2]}: ${match[3]}`;
+
+        // check for special syntax end
+        var endSequence = "";
+        if (opening.startsWith('{>>')) { 
+          endSequence = '<<}';
+        }
+        else if (opening.startsWith('(')) {
+          endSequence = ')';
+        }
+        else if (opening.startsWith('[')) {
+          endSequence = ']';
+        }
+        if (endSequence.length > 0) {
+          var endIndex = tMessage.indexOf(endSequence);
+          if (endIndex >= 0) {
+            tMessage = tMessage.substring(0, endIndex).trim();
+          }
+        }
+
         diagnostics.push({
-          range: new vscode.Range(lineCount, rangeStart, lineCount, rangeStart+rangeLength),
+          range: new vscode.Range(lineCount, rangeStart, lineCount, rangeStart+tMessage.length),
           message: tMessage,
           severity: tSeverity,
           source: 'custom-md',
